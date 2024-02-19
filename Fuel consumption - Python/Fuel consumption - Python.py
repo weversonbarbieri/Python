05e4eac7ac74827602d987003b628c22deeaa505
weversonbarbieri <weverson.barbieri@gmail.com>
2024-02-19 19:13:55 -0400

    Fuel consumption - Python
    Enter the distance driven and the fuel spent, this algorithm calculates the average fuel spent.

---
+++ Fuel consumption - Python/Fuel consumption - Python.py
@@ -0,0 +1,7 @@
+
+distance = int(input("Enter the distance driven: "))
+fuelspent = float(input("Enter the fuel spent: "))
+
+averagefuel = distance / fuelspent
+
+print(f"Average fuel consumption: {averagefuel:.3f}")
