bb15fddd430c0564886a119e88bf05a01b2f6546
weversonbarbieri <weverson.barbieri@gmail.com>
2024-02-19 19:30:26 -0400

    Square triangle trapeze area - Python
    Enter value A, B and C, the algorithm calculates the trapeze, square and triangle areas.

---
+++ Square triangle trapeze area - Python/Square triangle trapeze area.py
@@ -0,0 +1,12 @@
+
+a = float(input("Enter A value: "))
+b = float(input("Enter B value: "))
+c = float(input("Enter C value: "))
+
+squareare = pow(a, 2)
+trianglearea = (a * b) / 2
+trapezearea = ((a + b) * c) / 2
+
+print(f"SQUARE AREA = {squareare:.4f}")
+print(f"TRIANGLE AREA = {trianglearea:.4f}")
+print(f"TRAPEZE AREA = {trapezearea:.4f}")
